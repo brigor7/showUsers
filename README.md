04238e7fffd23d355e66a7b989646e133b89cc1d
=Bruno Igor <=brigor7@gmail.com>
2020-06-16 14:46:29 -0300

    Construção de API - mostrar Usuários

---
+++ README.md
@@ -0,0 +1,34 @@
+## Show Users
+
+Page with show users
+
+## Objective
+
+Construção de página para trabalhar com os components:
+
+- componentDidMount() - Utilizado uma vez pela aplicaçãoyar
+- componentDidUpdate()
+- componentWillUnmount()
+
+## Technologies
+
+-Javascript
+-React
+-NodeJS
+-Materialize
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+## View Page
+
+![](https://github.com/brigor7/buttonsIncrementDecrement/review.jpg);
---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import ProjetoBase from './components/ProjetoBase/ProjetoBase';
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      users: [],
+      showUsers: false,
+    };
+  }
+  async componentDidMount() {
+    const res = await fetch(
+      'https://randomuser.me/api/?seed=rush&nat=br&results=10'
+    );
+    const json = await res.json();
+    console.log(json);
+
+    this.setState({
+      users: json.results,
+    });
+    console.log('componentDidMount de APP.js');
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate de APP.js');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount de app.js');
+  }
+
+  handleShowUsers = (event) => {
+    this.setState({ showUsers: event.target.checked });
+  };
+
+  render() {
+    const { showUsers } = this.state;
+    console.log(showUsers);
+    return (
+      <div>
+        <div className="switch">
+          <label>
+            Mostrar usuários:
+            <input type="checkbox" onChange={this.handleShowUsers} />
+            <span className="lever" />
+          </label>
+        </div>
+        <hr />
+        {showUsers ? <div>Users</div> : <div>Não há usuários!</div>}
+      </div>
+    );
+  }
+}
